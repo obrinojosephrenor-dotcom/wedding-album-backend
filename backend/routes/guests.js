d74c09a2e4b1a4106580bcec99eb95adae426e04
Joseph Renor Obrino <obrino.josephrenor@nwssu.edu.ph>
2026-06-19 20:11:06 +0800

    Flatten backend structure

--- backend/routes/guests.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { registerGuest, getGuestById } from "../controllers/guestController.js";
-
-const router = Router();
-
-router.post("/register", registerGuest);
-router.get("/:id",       getGuestById);
-
-export default router;
